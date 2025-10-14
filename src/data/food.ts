f87a9040f4ca45c1cd7f3ddb11c2e4c1023ee6ca
PEDENAUD <laura.pedenaud-ext@rexel.com>
2025-10-14 16:46:07 +0200

    feat: add reset button to calculator

--- src/data/food.ts
+++ src/data/food.ts
@@ -14,7 +14,7 @@ export interface FoodItem {
 // values from: http://dieteplaisir.canalblog.com/archives/2012/05/18/24268064.html
 export const FOOD_ITEMS: Array<FoodItem> = [
   {
-    name: "Sushi",
+    name: "Rice to meet u",
     kcal: 45,
     imageUrl: Sushi
   },
